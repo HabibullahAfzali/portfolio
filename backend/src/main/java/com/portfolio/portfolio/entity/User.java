a85fa9c81ba64100583e1154234e68bd3b22d28b
HabibAfzali <82744410+HabibullahAfzali@users.noreply.github.com>
2025-01-30 18:10:56 +0100

    geter and seter added to user entiy

--- backend/src/main/java/com/portfolio/portfolio/entity/User.java
+++ backend/src/main/java/com/portfolio/portfolio/entity/User.java
@@ -1,11 +1,15 @@
 package com.portfolio.portfolio.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
